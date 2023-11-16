c59c9bfba0aa79dc8a8f85210d70badb6a88d804
NotCodeium <poopmastah1334@gmail.com>
2023-11-15 20:47:40 -0500

    done I think

--- exercises/19-targeting-dom/targeting-dom.js
+++ exercises/19-targeting-dom/targeting-dom.js
@@ -11,5 +11,36 @@
  * Do not change the HTML unless you are instructed to do so.
  */
 (function () {
-  // Put your answers in here
+  // Put your answers in here  var firstLi = document.querySelector('section:nth-child(2) ul li');
+  let firstLi = document.querySelector('section:nth-child(2) ul li');
+  let elementWithId = document.getElementById('myId');
+  
+  console.log('Single Elements:');
+  console.log(firstLi);
+  console.log(elementWithId);
+
+
+  let yellowBackgroundLiElements = document.querySelectorAll('section:nth-child(3) ul li.bg-warning');
+
+  console.log('\nGroups of Elements:');
+  yellowBackgroundLiElements.forEach(function (element) {
+    console.log(element);
+  });
+
+  let targetButton = document.getElementById('targetButton');
+  let socialMediaLinks = document.querySelectorAll('section:nth-child(4) a.social-link');
+
+  console.log('\nStructuring HTML for JavaScript:');
+  console.log(targetButton);
+  socialMediaLinks.forEach(function (element) {
+    console.log(element);
+  });
+
+
+  let cellsInMyRow = document.querySelectorAll('section:nth-child(5) #myRow div.border');
+
+  console.log('\nNested Elements:');
+  cellsInMyRow.forEach(function (element) {
+    console.log(element);
+  });
 })();
